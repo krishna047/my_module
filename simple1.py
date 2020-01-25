f0e6d091d68c74fc46fbfdcfca613b010c0f1689
krishn047 <krishnamoorthy047@gmail.com>
2020-01-25 18:05:35 +0100

    Included new files

---
+++ simple1.py
@@ -0,0 +1,13 @@
+'''
+A Very simple script
+'''
+
+def myfunc():
+    '''
+    A simple script
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+myfunc()  #Executing the function
